f019b7099559ef49f683e7f587402004e7b24aac
Yashido <jbpablico27@gmail.com>
2022-07-30 13:30:15 -0700

    More elements added

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ para.textContent = 'Hey I\'m red';
 para.style.color = 'red';
 
 container.appendChild(para);
+
+const headerthree = document.createElement('h3');
+headerthree.classList.add('hthree');
+headerthree.textContent = 'I\'m a blue h3!';
+headerthree.style.color = 'blue';
+
+container.appendChild(headerthree);
+
+const ncont = document.createElement('div');
+ncont.classList.add('conten');
+ncont.style.border = 2;
+ncont.style.borderColor = 'black';
+ncont.style.backgroundColor = 'pink';
+
+const paratwo = document.createElement('p');
+paratwo.classList.add
+
+
+
+
+container.appendChild(ncont);
